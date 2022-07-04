37504bd3c2ff17a5812360d33e59cf73b8b9a777
Neki Ram <nekiram@virtualemployee.com>
2022-07-04 18:09:54 +0530

    Order repo conflict resolved

--- ShoppingCart/Repositories/OrdersRepository.cs
+++ ShoppingCart/Repositories/OrdersRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿
+using Microsoft.EntityFrameworkCore;
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using ShoppingCart.DTOs;
@@ -8,21 +9,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 namespace ShoppingCart.Repositories
 {
-    public class OrdersRepository: IOrdersRepository
+    public class OrdersRepository : IOrdersRepository
     {
         private readonly ShoppingCartContext _context;
         private readonly IMapper _mapper;
-
 
         public OrdersRepository(ShoppingCartContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
         }
-
         public async Task AddToWishList(AddWishListDto wishlist)
         {
             var wishlistItem = new Wishlist
@@ -39,21 +37,19 @@ namespace ShoppingCart.Repositories
         public async Task<IEnumerable<WishlistItemDto>> GetWishListItemByUserId(int userId)
         {
             List<WishlistItemDto> _cart = new List<WishlistItemDto>();
-            
-            _cart = await(from c in _context.Wishlists
-                     join b in _context.Books on c.BookId equals b.BookId
-                     where c.UserId == userId
-                       select new WishlistItemDto
-                     {
-                         BookId=b.BookId,
-                         Image= ToBase64String(b.Image),
-                         BookName =b.Title,
-                         ProductType=b.ProductType,
-                         Price=b.OurPrice,
-                         Rating=b.Rating                         
-                     }).ToListAsync();
+            _cart = await (from c in _context.Wishlists
+                           join b in _context.Books on c.BookId equals b.BookId
+                           where c.UserId == userId
+                           select new WishlistItemDto
+                           {
+                               BookId = b.BookId,
+                               Image = ToBase64String(b.Image),
+                               BookName = b.Title,
+                               ProductType = b.ProductType,
+                               Price = b.OurPrice,
+                               Rating = b.Rating
+                           }).ToListAsync();
 
-            
             return _cart;
         }
         public async Task AddShippingDetails(ShippingDto shipping)
@@ -72,36 +68,26 @@ namespace ShoppingCart.Repositories
                     ZipCode = shipping.ZipCode,
                     Phone = shipping.Phone,
                     AddressType = shipping.AddressType,
-                    CreatedOn = DateTime.Now,
-                    CreatedBy = 1
+                    CreatedOn = shipping.CreatedOn,
+                    CreatedBy = shipping.CreatedBy
                 };
-
                 _context.Shippings.Add(shippingDetails);
                 await _context.SaveChangesAsync();
-            } 
-            catch(Exception ex)
-            {
-
             }
-            
-             
-            
+            catch (Exception ex)
+            {
+            }
         }
-
         public async Task<List<OrderDetailDto>> GetOrdersAsync()
         {
-
             var orderdetail = await _context.OrderDetails.ToListAsync();
             return _mapper.Map<List<OrderDetailDto>>(orderdetail);
         }
-
         public async Task<OrderDetailDto> GetOrderByIdAsync(int id)
         {
             var orderdetail = await _context.OrderDetails.FindAsync(id);
             return _mapper.Map<OrderDetailDto>(orderdetail);
-
         }
-
 
         public async Task<int> AddOrderAsync(OrderDetailDto orderdto)
         {
@@ -118,42 +104,7 @@ namespace ShoppingCart.Repositories
             _context.OrderDetails.Add(addorder);
             await _context.SaveChangesAsync();
             return addorder.OrderId;
-            }
-            
         }
-
-        public async Task CheckOut(decimal totalOrder)
-        {
-            try
-            {
-                decimal tax = .18m;
-                decimal shippingFee = 50.00m;
-                decimal totalTax = totalOrder * (tax);
-                decimal total = totalOrder + totalTax;
-                CheckOutDto checkOutDto = new CheckOutDto();
-
-                var checkout = new Checkout 
-                {
-                    CouponId = null,
-                    UserId = 1,
-                    Tax = totalTax,
-                    Shipping = shippingFee,
-                    FinalPay = total+ shippingFee,
-                    CreatedOn = DateTime.Now,
-                    CreatedBy = 1
-
-                };
-                _context.Checkouts.Add(checkout);
-                await _context.SaveChangesAsync();
-                
-            }
-            catch(Exception ex)
-            {
-
-            }
-
-        }
-
         public async Task UpdateOrderAsync(int id, OrderDetailDto orderdto)
         {
             var orderdetail = await _context.OrderDetails.FindAsync(id);
@@ -167,18 +118,13 @@ namespace ShoppingCart.Repositories
                 orderdetail.ModifiedBy = orderdto.ModifiedBy;
                 await _context.SaveChangesAsync();
             }
-
         }
-
-
-
         public async Task DeleteOrderAsync(int id)
         {
             var orderdetail = new OrderDetail() { OrderId = id };
             _context.OrderDetails.Remove(orderdetail);
             await _context.SaveChangesAsync();
         }
-
         public async Task<List<OrderStatusDto>> GetOrdersStatus()
         {
             var orderList = (from b in _context.Books
@@ -197,12 +143,41 @@ namespace ShoppingCart.Repositories
                                  ImageName = i.ImageName,
                                  CreatedBy = o.CreatedBy,
                                  ModifiedBy = o.ModifiedBy
-                             }).ToListAsync ();
-             return await orderList;
+                             }).ToListAsync();
+            return await orderList;
         }
 
+        public async Task CheckOut(decimal totalOrder)
+        {
+            try
+            {
+                decimal tax = .18m;
+                decimal shippingFee = 50.00m;
+                decimal totalTax = totalOrder * (tax);
+                decimal total = totalOrder + totalTax;
+                CheckOutDto checkOutDto = new CheckOutDto();
 
-       
+                var checkout = new Checkout
+                {
+                    CouponId = null,
+                    UserId = 1,
+                    Tax = totalTax,
+                    Shipping = shippingFee,
+                    FinalPay = total + shippingFee,
+                    CreatedOn = DateTime.Now,
+                    CreatedBy = 1
+
+                };
+                _context.Checkouts.Add(checkout);
+                await _context.SaveChangesAsync();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
         public static string ToBase64String(byte[] inArray)
         {
             string imgbase64 = "";
@@ -215,7 +190,6 @@ namespace ShoppingCart.Repositories
             return imgbase64;
         }
 
-        
-
     }
 }
+
